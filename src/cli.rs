d586fb60ae9c590b9abeef4bfe3c53adf2a5a415
Kern <mccoolname@gmail.com>
2023-11-19 21:14:47 -0800

    Record type argument is now optional (default A)

--- src/cli.rs
+++ src/cli.rs
@@ -3,10 +3,12 @@ use std::env;
 pub fn parse_args() -> (String, String) {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() != 3 {
-        eprintln!("Usage: kdnsq <record_type> <hostname>");
-        std::process::exit(1);
+    match args.len() {
+        2 => (String::from("A"), args[1].clone()),
+        3 => (args[1].to_uppercase(), args[2].clone()),
+        _ => {
+            eprintln!("Usage: kdnsq <record_type> <hostname>");
+            std::process::exit(1);
+        }
     }
-
-    (args[1].to_uppercase(), args[2].clone())
 }
